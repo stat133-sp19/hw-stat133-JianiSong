0056a0c61b5b1692c37066e42d2757eff5f686ba
Jiani Song <song_jiani@berkeley.edu>
2019-04-28 12:20:36 -0700

    Binomial package

---
+++ workout03/Binomial/tests/testthat/Test_Checker.R
@@ -0,0 +1,31 @@
+library(testthat)
+
+source("../../R/Binomial.R")
+
+context("tests for Checker functions")
+
+# Test check_prob
+test_that("Test if the check_prob works",{
+  expect_true(check_prob(0))
+  expect_length(check_prob(1/3),1)
+  expect_type(check_prob(1/3), "logical")
+
+} )
+
+# Test check_trials
+test_that("Test if the check_trials works",{
+  expect_true(check_trials(5))
+  expect_length(check_trials(5),1)
+  expect_type(check_trials(10),"logical")
+
+})
+
+# Test check_success
+test_that("Test if the check_success works",{
+  expect_true(check_success(3,9))
+  expect_length( check_success(3,9),1)
+  expect_type(check_trials(10),"logical")
+
+} )
+
+
